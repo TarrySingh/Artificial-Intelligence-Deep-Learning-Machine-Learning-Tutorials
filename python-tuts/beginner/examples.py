49b697d6c459e8430a282059f0d4cb1689396ac0
tarry.singh <tarry.singh@gmail.com>
2017-08-04 20:40:32 +0200

    Update examples.py

--- python-tuts/beginner/examples.py
+++ python-tuts/beginner/examples.py
@@ -6,3 +6,24 @@
 ########################################################
 # / Some password generator example //
 ########################################################
+
+########################################################
+# Example 1 - Password Generator
+########################################################
+# 1 - Basic example
+
+import random
+import string
+
+# s = string.printable
+# passlen = 8
+#
+# p = "".join(random.sample(s,passlen))
+# print(p)
+
+# 2 - Somewhat more fun
+
+def pw_gen(size = 8, chars=string.printable):
+    return ''.join(random.choice(chars) for _ in range(size))
+# print('Password is '+ pw_gen())
+print('Password is '+ pw_gen(int(input('How many characters in your password?'))))
