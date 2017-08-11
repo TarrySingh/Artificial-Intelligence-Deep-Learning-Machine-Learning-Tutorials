7a89a449bd6db52f1bdb4d632ff7e20b5e6a7c3d
TarrySingh <tarry.singh@gmail.com>
2017-08-11 23:53:35 +0200

    Simple max of three example
    Baby example but keep practicing :)

--- python-tuts/beginner/examples.py
+++ python-tuts/beginner/examples.py
@@ -383,4 +383,29 @@ def fibo(num):
 num = int(input("Add a range of numbers: "))
 fibo(num)
 
+###########################################################
+# Example 7: Defining a maximum of three
+###########################################################
+
+# do the following
+# python maxofthree.py num1 num2 num3
+import sys
+
+if len(sys.argv) < 4:
+    print('Usage <value1> <value2> <value3>')
+    sys.exit ( 1 )
+
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+arg3 = sys.argv[3]
+def maxfunction(a,b,c):
+    if (a > b) and (a > c):
+        print('Max value is :',a)
+    elif (b > a) and (b > c):
+        print('Max value is :',b)
+    elif (c > a) and (c > b):
+        print('Max value is :',c)
+
+maxfunction(arg1,arg2,arg3)
+
 
