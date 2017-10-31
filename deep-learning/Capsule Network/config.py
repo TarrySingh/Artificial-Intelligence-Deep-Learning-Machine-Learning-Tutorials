0092e356baa15fb1b70e6d91dce2479144a6db6f
TarrySingh <tarry.singh@gmail.com>
2017-10-31 22:46:45 +0100

    Capsule Networks
    CapsNets might trump CNNs.

---
+++ deep-learning/Capsule Network/config.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+flags = tf.app.flags
+
+
+############################
+#    hyper parameters      #
+############################
+
+# For separate margin loss
+flags.DEFINE_float('m_plus', 0.9, 'the parameter of m plus')
+flags.DEFINE_float('m_minus', 0.1, 'the parameter of m minus')
+flags.DEFINE_float('lambda_val', 0.5, 'down weight of the loss for absent digit classes')
+flags.DEFINE_integer('batch_size', 256, 'batch size')
+flags.DEFINE_integer('epoch', 2000, 'epoch')
+flags.DEFINE_integer('iter_routing', 3, 'number of iterations in routing algorithm')
+
+
+############################
+#   environment setting    #
+############################
+flags.DEFINE_string('dataset', 'data/mnist', 'the path for dataset')
+flags.DEFINE_boolean('is_training', True, 'train or predict phase')
+flags.DEFINE_integer('num_threads', 8, 'number of threads of enqueueing exampls')
+flags.DEFINE_string('logdir', 'logdir', 'logs directory')
+
+cfg = tf.app.flags.FLAGS
+tf.logging.set_verbosity(tf.logging.INFO)
