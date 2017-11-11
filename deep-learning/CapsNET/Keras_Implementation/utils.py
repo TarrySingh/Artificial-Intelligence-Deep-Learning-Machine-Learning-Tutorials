271126c8276f35df2f6031148dec7ee1da1b1a48
TarrySingh <tarry.singh@gmail.com>
2017-11-11 01:08:38 +0100

    CapsNet Keras Fashion MNIST Testing
    After a bit of struggle making this thing work. Running 100 epochs
    right now. Later maybe try 500 or so, however results from others show
    that this one eventually chokes at ≈ 95%.

---
+++ deep-learning/CapsNET/Keras_Implementation/utils.py
@@ -0,0 +1,61 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import csv
+import math
+
+def plot_log(filename, show=True):
+    # load data
+    keys = []
+    values = []
+    with open(filename, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if keys == []:
+                for key, value in row.items():
+                    keys.append(key)
+                    values.append(float(value))
+                continue
+
+            for _, value in row.items():
+                values.append(float(value))
+
+        values = np.reshape(values, newshape=(-1, len(keys)))
+        values[:,0] += 1
+
+    fig = plt.figure(figsize=(4,6))
+    fig.subplots_adjust(top=0.95, bottom=0.05, right=0.95)
+    fig.add_subplot(211)
+    for i, key in enumerate(keys):
+        if key.find('loss') >= 0 and not key.find('val') >= 0:  # training loss
+            plt.plot(values[:, 0], values[:, i], label=key)
+    plt.legend()
+    plt.title('Training loss')
+
+    fig.add_subplot(212)
+    for i, key in enumerate(keys):
+        if key.find('acc') >= 0:  # acc
+            plt.plot(values[:, 0], values[:, i], label=key)
+    plt.legend()
+    plt.title('Training and validation accuracy')
+
+    # fig.savefig('result/log.png')
+    if show:
+        plt.show()
+
+
+def combine_images(generated_images):
+    num = generated_images.shape[0]
+    width = int(math.sqrt(num))
+    height = int(math.ceil(float(num)/width))
+    shape = generated_images.shape[1:3]
+    image = np.zeros((height*shape[0], width*shape[1]),
+                     dtype=generated_images.dtype)
+    for index, img in enumerate(generated_images):
+        i = int(index/width)
+        j = index % width
+        image[i*shape[0]:(i+1)*shape[0], j*shape[1]:(j+1)*shape[1]] = \
+            img[:, :, 0]
+    return image
+
+if __name__=="__main__":
+    plot_log('result/log.csv')
