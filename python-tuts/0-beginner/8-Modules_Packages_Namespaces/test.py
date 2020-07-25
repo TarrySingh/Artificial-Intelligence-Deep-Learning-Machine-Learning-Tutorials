2cc86d579f7ab9e948d077d50e9ecf5cb0a37898
Tarry Singh <tarry.singh@gmail.com>
2020-07-25 19:08:09 +0200

    reloading modules

---
+++ python-tuts/0-beginner/8-Modules_Packages_Namespaces/test.py
@@ -0,0 +1,9 @@
+# test.py
+
+print('running test.py...')
+
+def print_values():
+	print('k1', '10')
+	print('k2', 'python')
+	print('k3', 'cheese')
+	print('k4', 'parrots')
---
+++ python-tuts/0-beginner/8-Modules_Packages_Namespaces/test2.py
@@ -0,0 +1,7 @@
+# test2.py
+
+print('running test2.py...')
+
+def print_values():
+	print('k1', 'python')
+	print('k2', 'cheese')
