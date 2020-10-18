3c7005940282776abb3b554b1e836046eccef67c
Tarry Singh <tarry.singh@gmail.com>
2020-10-18 20:24:49 +0200

    comm

---
+++ python-tuts/1-intermediate/04 - Iteration tools/Project /project_4_goal_2/playground.py
@@ -0,0 +1,26 @@
+import itertools
+
+import constants
+import parse_utils
+
+# for fname, class_name, parser in zip(constants.fnames, constants.class_names, constants.parsers):
+#     file_iter = parse_utils.iter_file(fname, class_name, parser)
+#     print(fname)
+#     for _ in range(3):
+#         print(next(file_iter))
+#     print()
+
+# gen = parse_utils.iter_combined_plain_tuple(constants.fnames, constants.class_names,
+#                                             constants.parsers, constants.compress_fields)
+#
+# print(list(next(gen)))
+# print(list(next(gen)))
+
+# nt = parse_utils.create_combo_named_tuple_class(constants.fnames, constants.compress_fields)
+# print(nt._fields)
+
+data_iter = parse_utils.iter_combined(constants.fnames, constants.class_names,
+                                      constants.parsers, constants.compress_fields)
+
+for row in itertools.islice(data_iter, 5):
+    print(row)
