b5abd79ac414fb36379d6df81b7bd6454b1cdd18
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 17:20:35 +0000

    Adding numeric_owner as keyword arguement

--- deep-learning/multi-frameworks/common/utils.py
+++ deep-learning/multi-frameworks/common/utils.py
@@ -183,7 +183,7 @@ def maybe_download_cifar(src="https://ikpublictutorial.blob.core.windows.net/dee
             		if not is_within_directory(path, member_path):
             			raise Exception("Attempted Path Traversal in Tar File")
             
-            	tar.extractall(path, members, numeric_owner) 
+            	tar.extractall(path, members, numeric_owner=numeric_owner) 
             	
             
             safe_extract(tar)
